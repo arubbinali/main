4dd2b87eb3ae8102757baadb039cb92deeac8e97
Arub bin Ali <arubbinali@outlook.com>
2025-11-02 00:59:15 +0300

    `

--- main/src/App.js
+++ main/src/App.js
@@ -9,30 +9,31 @@ function App() {
         raysOrigin="bottom-center"
         raysColor="#ffffff"
         raysSpeed={1.5}
-        lightSpread={0.8}
-        rayLength={1.2}
+        lightSpread={window.innerWidth < 1000 ? 9999 : 0.8} // 👈 wider spread on small screens
+        rayLength={window.innerWidth < 1000 ? 9999 : 3}   // 👈 longer rays on mobile
         followMouse={true}
         mouseInfluence={0.1}
         noiseAmount={0.1}
         distortion={0.0}
         className="custom-rays"
-        style={{ zIndex: 1 }} // 👈 stays below
+        style={{ zIndex: 1 }}
       />
+
 
     {/* Centered Text */}
       <div className="center-text">
         <h1>Welcome to My New Page, It's still in the making</h1>
         <p>
-  You can find my current site{" "}
-  <a
-    href="https://arubbinali.github.io/"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="temp-link"
-  >
-    here
-  </a>
-</p>
+          You can find my current site{" "}
+          <a
+            href="https://arubbinali.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="temp-link"
+          >
+            here
+          </a>
+        </p>
 
 
       </div>
