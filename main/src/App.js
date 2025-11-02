882e700c4e4700077b7d35c4ad57bf98e4794ba8
Arub bin Ali <arubbinali@outlook.com>
2025-11-03 00:34:58 +0300

    `

--- main/src/App.js
+++ main/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Main from "./pages/main";
 import About from "./pages/about";
 
