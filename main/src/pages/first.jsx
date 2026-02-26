df12aa30916bf57a4e7a224734705aee757bb046
Arub bin Ali <arubbinali@outlook.com>
2026-02-26 12:48:36 +0800

    `

--- main/src/pages/first.jsx
+++ main/src/pages/first.jsx
@@ -11,24 +11,24 @@ function First() {
     }}>
       <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
         <GridScan
-          sensitivity={0.55}
-          lineThickness={1}
+          sensitivity={0.15}
+          lineThickness={0.1}
           linesColor="#392e4e"
           gridScale={0.1}
           scanColor="#FF9FFC"
-          scanOpacity={0.4}
+          scanOpacity={1}
           lineStyle="solid"
-          lineJitter={0.1}
+          lineJitter={1}
           scanDirection="pingpong"
           enablePost
           bloomIntensity={0.6}
           chromaticAberration={0.002}
           noiseIntensity={0.01}
-          scanGlow={0.5}
+          scanGlow={0.4}
           scanSoftness={2}
           scanPhaseTaper={0.9}
-          scanDuration={2.0}
-          scanDelay={2.0}
+          scanDuration={4.0}
+          scanDelay={1.0}
         />
       </div>
 
