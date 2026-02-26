98087ba537d2dbc1e6fe8e682949105398078bdf
Arub bin Ali <arubbinali@outlook.com>
2026-02-26 12:54:09 +0800

    `

--- main/src/App.js
+++ main/src/App.js
@@ -2,6 +2,7 @@ import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Main from "./pages/main";
 import About from "./pages/about";
 import First from "./pages/first";
+import Lit from "./pages/lit";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
   <Route path="/main" element={<Main />} />
   <Route path="/about" element={<About />} />
   <Route path="/first" element={<First />} />
+  <Route path="/lit" element={<Lit />} />
         {/* Redirect any unknown path to /main */}
         <Route path="*" element={<Navigate to="/main" replace />} />
       </Routes>
